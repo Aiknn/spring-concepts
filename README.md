6931d37a86bcd3c2441c3e56960dd17710aa8a8b
Yerlan Nurlibayev <ynurlibayev@outlook.com>
2022-09-09 18:46:38 +0600

    feat: add new info

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Spring Concepts
 
-### All information in the next branches:
+### di-without-spring
+* In this part we used Dependency Injection without Spring and its annotations.
+* We used 3 types of DI. But constructor injection is mostly preferred.
 
-1. spring-context
-2. di-without-spring
-3. di-spring
-4. asd
-5. asd
+### di-spring
+* We put @Controller annotation (it extends from @Component), so Spring retrieve bean. 
+* The beans name equals annotated class name (MyController) with first small letter (myController). 
+* As you can see in this example, main class could create object from MyController class without using 'new'.
+* We put @Autowired annotation to injected class, so Spring will inject it when created object from main class.
+* We put @Service annotation to implemented class (from interface), so Spring know which implementation of interface to use.
--- src/main/java/kz/aiknn/springconcepts/SpringConceptsApplication.java
+++ src/main/java/kz/aiknn/springconcepts/SpringConceptsApplication.java
@@ -1,13 +1,52 @@
 package kz.aiknn.springconcepts;
 
+import kz.aiknn.springconcepts.controllers.ConstructorInjectedController;
+import kz.aiknn.springconcepts.controllers.PropertyInjectedController;
+import kz.aiknn.springconcepts.controllers.SetterInjectedController;
+import kz.aiknn.springconcepts.services.GreetingServiceKazakh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
 
 	public static void main(String[] args) {
+		/*
+		System.out.println("Without Spring - to activate you need uncomment and delete spring code below the code");
+
 		SpringApplication.run(SpringConceptsApplication.class, args);
+
+		PropertyInjectedController controller = new PropertyInjectedController();
+		controller.greetingService = new GreetingServiceKazakh();
+		System.out.println(controller.getGreeting());
+
+		SetterInjectedController controller1 = new SetterInjectedController();
+		controller1.setGreetingService(new GreetingServiceKazakh());
+		System.out.println(controller1.getGreeting());
+
+		ConstructorInjectedController controller2 =
+				new ConstructorInjectedController(new GreetingServiceKazakh());
+		System.out.println(controller2.getGreeting());
+		*/
+
+		ApplicationContext ctx = SpringApplication.run(SpringConceptsApplication.class, args);
+
+		System.out.println("Property injected controller");
+		PropertyInjectedController controller = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(controller.getGreeting());
+		System.out.println("---------------------------");
+
+		System.out.println("Setter injected controller");
+		SetterInjectedController controller1 = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(controller1.getGreeting());
+		System.out.println("---------------------------");
+
+		System.out.println("Constructor injected controller");
+		ConstructorInjectedController controller2 = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(controller2.getGreeting());
+		System.out.println("---------------------------");
+
 	}
 
 }
